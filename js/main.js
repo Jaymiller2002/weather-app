a710e93991e27e23bcedfe55f985406444731600
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-10 16:57:34 -0400

    .

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Define the API URL
-const i = 'https://api.openweathermap.org/data/2.5/weather?q={Lexington}&appid={APIkey}';
+const i = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}';
 const APIKey = '8ff8627e5760145923c8962bd8e95052';
 // Make a GET request
 fetch(i)
