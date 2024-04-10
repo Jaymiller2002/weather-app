ac8ff109d934f864e1fc58beff074307008768dd
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-10 13:57:03 -0400

    js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+// Define the API URL
+const apiUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={9fd938103ec5b5fb92be500ba2c85323}';
+
+// Make a GET request
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
