614bb25b61eae644eeb2e01f3934a67b2f4ed0d5
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-10 15:48:06 -0400

    js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // Define the API URL
-const apiUrl = 'https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={38.0406}&lon={-84.5037}&appid={9fd938103ec5b5fb92be500ba2c85323}';
+const i = 'https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={38.0406}&lon={-84.5037}&appid={9fd938103ec5b5fb92be500ba2c85323}';
 
 // Make a GET request
-fetch(apiUrl)
+fetch(i)
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok');
