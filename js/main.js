86f5ede3ab63f74779e445b60fc3ea200a6a4da9
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-10 14:14:34 -0400

    js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Define the API URL
-const apiUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={9fd938103ec5b5fb92be500ba2c85323}';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={9fd938103ec5b5fb92be500ba2c85323}';
 
 // Make a GET request
 fetch(apiUrl)
