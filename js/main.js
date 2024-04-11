83f739cf0f59155d82e39ebe398c6e81dd4a8b14
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-10 19:54:38 -0700

    .

--- js/main.js
+++ js/main.js
@@ -3,14 +3,23 @@ const i = 'http://api.openweathermap.org/geo/1.0/direct?q=Lexington,KY,US&limit=
 // Make a GET request
 fetch(i)
   .then(response => {
+    //Check if the request was succesful
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
     return response.json();
   })
   .then(data => {
-    console.log(data);
+    // Parse  the data into a javascript object
+    const parsedData = JSON.parse(data);
+    // Use the javascript object to create HTML elements
+    parsedData.forEach(item => {
+      const div = document.createElement('div');
+      div.textContent = item.name;
+      // Append the new elements to the DOM
+      document.body.appendChild(div);
+    });
   })
   .catch(error => {
-    console.log('Error:', error);
+    console.error('There has been a problem with your fetch operation:', error);
   });
