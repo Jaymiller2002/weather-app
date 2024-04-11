67c95586d4f33429afec4df8d1516c27758da4c9
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-11 13:10:55 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const buttonTemp = document.getElementById('buttonTemp');
 const buttonWeather = document.getElementById('buttonWeather');
-const buttonLatLon = document.getElementById('buttonLatLon');
+const buttonLatitude = document.getElementById('buttonLatitude');
+const buttonLongitude = document.getElementById('buttonLongitude');
 // Define the API URL
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=40513,us&appid=9fd938103ec5b5fb92be500ba2c85323';
 // Make a GET request
@@ -19,12 +20,14 @@ fetch(apiUrl)
     //If getting info, parse the data into a javascript object
     //const Data = JSON.parse(data);
     // Use the javascript object to create HTML elements
-    console.log(data.main.feels_like);
-    buttonTemp.textContent = data.main.feels_like;
-    console.log(data.wind.speed);
-    buttonWeather.textContent = data.wind.speed;
+    console.log(data.main.temp);
+    buttonTemp.textContent = data.main.temp;
+    console.log(data.weather[0].description);
+    buttonWeather.textContent = data.weather[0].description;
     console.log(data.coord.lat);
-    buttonLatLon.textContent = data.coord.lat;
+    buttonLatitude.textContent = data.coord.lat;
+    console.log(data.coord.lon);
+    buttonLongitude.textContent = data.coord.lon;
     // item => 
     //  const div = document.createElement('div');
     //   div.textContent = item.name;
